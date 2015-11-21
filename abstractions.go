340e798faee70274c3f3a9ab1eb426ea01a88e8b
Alex Nichol <unixpickle@gmail.com>
2015-11-21 16:28:53 -0500

    implemented Syncer

--- abstractions.go
+++ abstractions.go
@@ -1,15 +1,34 @@
 package dirsync
 
+import "path"
+
 type FileInfo struct {
-	IsDir    bool
-	Path     string
+	// IsDir is true if and only if the remote file is a directory.
+	IsDir bool
+
+	// Path is a slash-separated path in the remote filesystem.
+	Path string
+
+	// FileSize is the size of the file (in bytes).
+	// This field is not used for directories.
 	FileSize uint64
 }
 
+// Name returns the basename of the path.
+func (f FileInfo) Name() string {
+	return path.Base(f.Path)
+}
+
+// A Lister provides remote directory listings.
 type Lister interface {
+	// List reads the contents of a remote (slash-separated) path.
+	// If an error occurs, this should return nil, err.
 	List(path string) ([]FileInfo, error)
 }
 
+// A Downloader downloads remote files (but not directories).
 type Downloader interface {
+	// Download the file located at a remote (slash-separated) path.
+	// The file should be saved at the given local path.
 	Download(remotePath, localPath string) error
 }
---
+++ syncer.go
@@ -0,0 +1,160 @@
+package dirsync
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+// A Syncer keeps a local folder up to date with a remote server.
+type Syncer struct {
+	LocalPath  string
+	RemotePath string
+
+	Lister     Lister
+	Downloader Downloader
+
+	Interval time.Duration
+}
+
+// Sync runs an infinite sync loop.
+// The loop reads remote listings, downloads any locally missing files, and deletes any
+// extraneous local files.
+// If a synchronization fails, this will return the error.
+func (s Syncer) Sync() error {
+	for {
+		nextTimeout := time.After(s.Interval)
+		if err := s.syncOnce(); err != nil {
+			return err
+		}
+		<-nextTimeout
+	}
+}
+
+// syncOnce performs one sync iteration.
+// This returns the first error that occurs.
+func (s Syncer) syncOnce() error {
+	a, err := s.computeAgenda()
+	if err != nil {
+		return err
+	}
+
+	for _, path := range a.delete {
+		if err := os.RemoveAll(path); err != nil {
+			return err
+		}
+	}
+
+	for i, remote := range a.download {
+		if err := s.download(remote, a.downloadDest[i]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// download recursively copies a remote directory to the local path.
+func (s Syncer) download(remote FileInfo, local string) error {
+	if !remote.IsDir {
+		return s.Downloader.Download(remote.Path, local)
+	}
+
+	listing, err := s.Lister.List(remote.Path)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range listing {
+		dest := filepath.Join(local, entry.Name())
+		if err := s.download(entry, dest); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// agenda is a list of things which need to be deleted and downloaded.
+type agenda struct {
+	delete       []string
+	download     []FileInfo
+	downloadDest []string
+}
+
+type agendaSearchNode struct {
+	localPath  string
+	remotePath string
+}
+
+// computeAgenda generates an agenda to correct the differences between
+// the local and remote directory.
+func (s Syncer) computeAgenda() (*agenda, error) {
+	agenda := &agenda{[]string{}, []FileInfo{}, []string{}}
+
+	// Perform a breadth-first search over the remote directory.
+	nodes := []agendaSearchNode{{s.LocalPath, s.RemotePath}}
+	for len(nodes) > 0 {
+		n := nodes[0]
+		nodes = nodes[1:]
+
+		localListing, err := ioutil.ReadDir(n.localPath)
+		if err != nil {
+			return nil, err
+		}
+		remoteListing, err := s.Lister.List(n.remotePath)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, local := range localListing {
+			var remoteMatch *FileInfo
+			for _, remote := range remoteListing {
+				if localRemoteFilesEqual(local, remote) {
+					remoteMatch = &remote
+					break
+				}
+			}
+			localPath := filepath.Join(n.localPath, local.Name())
+			if remoteMatch == nil {
+				agenda.delete = append(agenda.delete, localPath)
+			} else if local.IsDir() {
+				node := agendaSearchNode{
+					localPath:  localPath,
+					remotePath: remoteMatch.Path,
+				}
+				nodes = append(nodes, node)
+			}
+		}
+
+		for _, remote := range remoteListing {
+			foundMatch := false
+			for _, local := range localListing {
+				if localRemoteFilesEqual(local, remote) {
+					foundMatch = true
+					break
+				}
+			}
+			if !foundMatch {
+				agenda.download = append(agenda.download, remote)
+				dest := filepath.Join(n.localPath, remote.Name())
+				agenda.downloadDest = append(agenda.downloadDest, dest)
+			}
+		}
+	}
+
+	return agenda, nil
+}
+
+func localRemoteFilesEqual(local os.FileInfo, remote FileInfo) bool {
+	if local.Name() != remote.Name() {
+		return false
+	}
+	if local.IsDir() != remote.IsDir {
+		return false
+	}
+	if !local.IsDir() && uint64(local.Size()) != remote.FileSize {
+		return false
+	}
+	return true
+}
